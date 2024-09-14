36f2cfb94d05ae27f5ca94d33e54399737c6f526
sheveykoandrey <77dream3r@gmail.com>
2024-09-14 05:15:53 +0300

    Update Main.java

--- pract2_1/Main.java
+++ pract2_1/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Car car2 = new Car(); // Использование конструктора по умолчанию
         Car car3 = new Car("Honda Civic", 2018); // Использование конструктора с выбранными полями
 
-        // Вывод информации о машинах
+        // Вывод инфы о машинах
         System.out.println("Car 1: " + car1.getModel() + ", License: " + car1.getLicense() + ", Color: " + car1.getColor() + ", Year: " + car1.getYear());
         System.out.println("Car 2: " + car2.getModel() + ", License: " + car2.getLicense() + ", Color: " + car2.getColor() + ", Year: " + car2.getYear());
         System.out.println("Car 3: " + car3.getModel() + ", License: " + car3.getLicense() + ", Color: " + car3.getColor() + ", Year: " + car3.getYear());
